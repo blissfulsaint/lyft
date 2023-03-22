85debbff67ddd9b70f5b246207f513df0a2f4de4
hyrumParamore <97462630+hyrumParamore@users.noreply.github.com>
2023-03-22 12:53:34 -0600

    modal

--- src/main.js
+++ src/main.js
@@ -40,3 +40,12 @@ window.addEventListener("scroll", function () {
 });
 
 
+
+// Login Form Javascript
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
