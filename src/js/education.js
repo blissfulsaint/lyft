58c300f519ddad148509445742b0f0e47a0bcfe0
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-04-03 11:28:39 -0600

    final styles

--- src/js/education.js
+++ src/js/education.js
@@ -8,18 +8,18 @@ const container = document.querySelector('.cards');
 data.forEach((item) => {
   // Create card element
   const card = document.createElement('div');
-  card.classList = '.card';
+  card.classList = 'card';
 
   // Construct card content
   const content = 
-  `<div id="cardSection">
-        <div class="exerciseCard">
-        <img src="../images/lungesample.jpg" alt="">
-        <h2> ${item.name}</h2>
-        <h3> ${item.categories}</h3>
-        <p> ${item.description}</p>
-        </div>
-    </div>`;
+  `<div class="card">
+    <div class="img-container">
+      <img src="../images/lungesample.jpg" alt="">
+    </div>
+    <h2> ${item.name}</h2>
+    <h3> ${item.categories}</h3>
+    <p> ${item.description}</p>
+  </div>`;
 
   container.innerHTML += content;
 
