7a95c69b117bef34f8ba30932de9839041e52fc6
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-09-28 09:53:23 -0600

    updated stuff

--- src/js/education.js
+++ src/js/education.js
@@ -3,32 +3,34 @@
 
 
 
+async function init() {
+  let response = await fetch('https://lift-api.onrender.com/exercises/');
+  let data = await response.json();
 
-let response = await fetch('https://lift-api.onrender.com/exercises/');
-let data = await response.json();
+  const container = document.querySelector('.cards');
 
-const container = document.querySelector('.cards');
+  data.forEach((item) => {
+    // Create card element
+    const card = document.createElement('div');
+    card.classList = 'card';
 
-data.forEach((item) => {
-  // Create card element
-  const card = document.createElement('div');
-  card.classList = 'card';
+    // Construct card content
+    const content = 
+    `<div class="card">
+      <div class="img-container">
+        <img src="../images/lungesample.jpg" alt="">
+      </div>
+      <h2> ${item.name}</h2>
+      <h3> ${item.categories}</h3>
+      <p> ${item.description}</p>
+    </div>`;
 
-  // Construct card content
-  const content = 
-  `<div class="card">
-    <div class="img-container">
-      <img src="../images/lungesample.jpg" alt="">
-    </div>
-    <h2> ${item.name}</h2>
-    <h3> ${item.categories}</h3>
-    <p> ${item.description}</p>
-  </div>`;
+    container.innerHTML += content;
 
-  container.innerHTML += content;
+  });
+}
 
-});
-
+init();
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        cart: resolve(__dirname, 'src/cart/index.html'),
-        product: resolve(__dirname, 'src/product_pages/index.html'),
-        productListing: resolve(__dirname, 'src/product-listing/index.html'),
-        admin: resolve(__dirname, 'src/admin/index.html')
+        client: resolve(__dirname, 'src/client/index.html'),
+        education: resolve(__dirname, 'src/education/index.html')
       },
     },
   },
